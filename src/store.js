183498c75652588f12b761ddbf67c71579fa40b3
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-11-21 05:17:16 +0530

    Setup redux saga

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from "./reducers";
+import saga from "./sagas";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware, thunk));
+
+sagaMiddleware.run(saga);
 
 export default store;
