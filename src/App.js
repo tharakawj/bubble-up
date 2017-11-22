0a78e023bc3e0a2c524c5f7daff7b8a73e5d49c5
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-11-23 04:58:52 +0530

    Remove edit topic page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import store from "./store";
 import { fetchTopics } from "./actions";
 import HomePage from "./containers/HomePage";
 import NewTopic from "./containers/NewTopic";
-import EditTopic from "./containers/EditTopic";
 import NotFoundPage from "./containers/NotFoundPage";
 
 store.dispatch(fetchTopics());
@@ -20,7 +19,6 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={HomePage} />
               <Route path="/new" component={NewTopic} />
-              <Route path="/topics/:id" component={EditTopic} />
               <Route component={NotFoundPage} />
             </Switch>
           </div>
--- src/containers/EditTopic.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const EditTopic = () => <div>Edit Topic</div>;
-
-export default EditTopic;
