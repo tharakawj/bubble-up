258cc2323fa4dcda758996da4bbb7434cddec745
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-11-23 20:45:38 +0530

    Add new topic length validation

--- src/component/TopicForm.js
+++ src/component/TopicForm.js
@@ -1,22 +1,30 @@
 import React from "react";
+import { MAX_TOPIC_LENGTH } from "../constants/appConstants";
 
-const TopicForm = ({ text, disabled, buttonText, onChange, onSubmit }) => (
-  <form onSubmit={onSubmit}>
-    <textarea
-      className="form-control"
-      placeholder="Type your topic here..."
-      value={text}
-      onChange={onChange}
-      disabled={disabled}
-    />
-    <button
-      type="submit"
-      className="btn btn-primary pull-right submit-button"
-      disabled={disabled}
-    >
-      {buttonText}
-    </button>
-  </form>
-);
+const TopicForm = ({ text, disabled, buttonText, onChange, onSubmit }) => {
+  const submitDisabled =
+    disabled || text.length === 0 || text.length > MAX_TOPIC_LENGTH;
+  return (
+    <form onSubmit={onSubmit}>
+      <textarea
+        className="form-control"
+        placeholder="Type your topic here..."
+        value={text}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <span
+        className={text.length > MAX_TOPIC_LENGTH ? "error" : ""}
+      >{`${text.length}/${MAX_TOPIC_LENGTH}`}</span>
+      <button
+        type="submit"
+        className="btn btn-primary pull-right submit-button"
+        disabled={submitDisabled}
+      >
+        {buttonText}
+      </button>
+    </form>
+  );
+};
 
 export default TopicForm;
--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,1 +1,2 @@
 export const TOPICS_IN_HOMEPAGE = 20;
+export const MAX_TOPIC_LENGTH = 255;
